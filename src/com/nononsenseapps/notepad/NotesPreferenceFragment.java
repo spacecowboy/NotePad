ef27c204974fbfce27a778c4d98576c53f396fdc
Jonas Kalderstam <jonask@thep.lu.se>
2012-01-25 10:13:15 +0100

    2.1.1 - fixes #35 where you could crash the app by exiting settings too quick after a change.

--- src/com/nononsenseapps/notepad/NotesPreferenceFragment.java
+++ src/com/nononsenseapps/notepad/NotesPreferenceFragment.java
@@ -1,5 +1,6 @@
 package com.nononsenseapps.notepad;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -12,7 +13,7 @@ public class NotesPreferenceFragment extends PreferenceFragment implements
 	public static final String KEY_THEME = "key_current_theme";
 	public static final String KEY_SORT_ORDER = "key_sort_order";
 	public static final String KEY_SORT_TYPE = "key_sort_type";
-	
+
 	public static final String THEME_DARK = "dark";
 	public static final String THEME_LIGHT = "light";
 	public static final String THEME_LIGHT_ICS_AB = "light_ab";
@@ -24,6 +25,13 @@ public class NotesPreferenceFragment extends PreferenceFragment implements
 	public String SUMMARY_SORT_TYPE;
 	public String SUMMARY_SORT_ORDER;
 	public String SUMMARY_THEME;
+	private Activity activity;
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		this.activity = activity;
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,9 +45,9 @@ public class NotesPreferenceFragment extends PreferenceFragment implements
 
 		SUMMARY_SORT_ORDER = getText(R.string.settings_summary_sort_order_desc)
 				.toString();
-		
+
 		SUMMARY_THEME = getText(R.string.settings_summary_theme_dark)
-		.toString();
+				.toString();
 
 		prefSortOrder = getPreferenceScreen().findPreference(KEY_SORT_ORDER);
 		prefSortType = getPreferenceScreen().findPreference(KEY_SORT_TYPE);
@@ -62,17 +70,32 @@ public class NotesPreferenceFragment extends PreferenceFragment implements
 
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
 			String key) {
-		if (KEY_THEME.equals(key)) {
-			Log.d("settings", "Theme changed!");
-			setThemeSummary(sharedPreferences);
-		} else if (KEY_SORT_TYPE.equals(key)) {
-			Log.d("settings", "Sort type changed!");
-			setTypeSummary(sharedPreferences);
-		} else if (KEY_SORT_ORDER.equals(key)) {
-			Log.d("settings", "Sort order changed!");
-			setOrderSummary(sharedPreferences);
-		} else
-			Log.d("settings", "Somethign changed!");
+		try {
+			if (activity.isFinishing()) {
+				Log.d("settings", "isFinishing, should no update summaries");
+				// Setting the summary now would crash it with
+				// IllegalStateException since we are not attached to a view
+			} else {
+				if (KEY_THEME.equals(key)) {
+					Log.d("settings", "Theme changed!");
+					setThemeSummary(sharedPreferences);
+				} else if (KEY_SORT_TYPE.equals(key)) {
+					Log.d("settings", "Sort type changed!");
+					setTypeSummary(sharedPreferences);
+				} else if (KEY_SORT_ORDER.equals(key)) {
+					Log.d("settings", "Sort order changed!");
+					setOrderSummary(sharedPreferences);
+				} else
+					Log.d("settings", "Somethign changed!");
+			}
+		} catch (IllegalStateException e) {
+			// This is just in case the "isFinishing" wouldn't be enough
+			// The isFinishing will try to prevent us from doing something
+			// stupid
+			// This catch prevents the app from crashing if we do something
+			// stupid
+			Log.d("settings", "Exception was caught: " + e.getMessage());
+		}
 	}
 
 	private void setOrderSummary(SharedPreferences sharedPreferences) {
@@ -102,22 +125,20 @@ public class NotesPreferenceFragment extends PreferenceFragment implements
 		SUMMARY_SORT_TYPE = summary;
 		prefSortType.setSummary(summary);
 	}
-	
+
 	private void setThemeSummary(SharedPreferences sharedPreferences) {
 		// Dark theme is default
-		String value = sharedPreferences.getString(KEY_THEME,
-				THEME_DARK);
+		String value = sharedPreferences.getString(KEY_THEME, THEME_DARK);
 		String summary;
 		if (THEME_DARK.equals(value))
-			summary = getText(R.string.settings_summary_theme_dark)
-					.toString();
+			summary = getText(R.string.settings_summary_theme_dark).toString();
 		else if (THEME_LIGHT.equals(value))
-			summary = getText(R.string.settings_summary_theme_light)
-			.toString();
+			summary = getText(R.string.settings_summary_theme_light).toString();
 		else
 			summary = getText(R.string.settings_summary_theme_light_dark_ab)
-			.toString();
+					.toString();
 		SUMMARY_THEME = summary;
+		Log.d("setThemeSummary", "Setting summary now");
 		prefTheme.setSummary(summary);
 	}
 
