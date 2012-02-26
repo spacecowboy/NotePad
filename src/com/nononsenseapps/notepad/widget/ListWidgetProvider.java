2313e5775aadd850682a8800bc5f6c669448bb89
SciencyGuy <jonas@kalderstam.se>
2012-02-26 23:31:14 +0100

    Now opens app on title click
    Signed-off-by: SciencyGuy <jonas@kalderstam.se>

--- src/com/nononsenseapps/notepad/widget/ListWidgetProvider.java
+++ src/com/nononsenseapps/notepad/widget/ListWidgetProvider.java
@@ -21,49 +21,14 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ComponentName;
-import android.content.ContentValues;
 import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.database.Cursor;
-import android.database.ContentObserver;
 import android.net.Uri;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
-import java.util.Random;
-
-import com.nononsenseapps.notepad.NotePad;
+import com.nononsenseapps.notepad.FragmentLayout;
 import com.nononsenseapps.notepad.R;
-import com.nononsenseapps.notepad.NotePad.Notes;
-import com.nononsenseapps.notepad.R.id;
-import com.nononsenseapps.notepad.R.layout;
-
-/**
- * Our data observer just notifies an update for all weather widgets when it detects a change.
- */
-class WeatherDataProviderObserver extends ContentObserver {
-    private AppWidgetManager mAppWidgetManager;
-    private ComponentName mComponentName;
-
-    WeatherDataProviderObserver(AppWidgetManager mgr, ComponentName cn, Handler h) {
-        super(h);
-        mAppWidgetManager = mgr;
-        mComponentName = cn;
-    }
-
-    @Override
-    public void onChange(boolean selfChange) {
-        // The data has changed, so notify the widget that the collection view needs to be updated.
-        // In response, the factory's onDataSetChanged() will be called which will requery the
-        // cursor for the new data.
-        mAppWidgetManager.notifyAppWidgetViewDataChanged(
-                mAppWidgetManager.getAppWidgetIds(mComponentName), R.id.notes_list);
-    }
-}
 
 /**
  * The weather widget's AppWidgetProvider.
@@ -75,15 +40,7 @@ public class ListWidgetProvider extends AppWidgetProvider {
     public static String CREATE_ACTION = "com.nononsenseapps.notepad.widget.CREATE";
     public static String EXTRA_NOTE_ID = "com.nononsenseapps.notepad.widget.note_id";
 
-    private static HandlerThread sWorkerThread;
-    private static Handler sWorkerQueue;
-    private static WeatherDataProviderObserver sDataObserver;
-
     public ListWidgetProvider() {
-        // Start the worker thread
-        sWorkerThread = new HandlerThread("WeatherWidgetProvider-worker");
-        sWorkerThread.start();
-        sWorkerQueue = new Handler(sWorkerThread.getLooper());
     }
 
     @Override
@@ -92,23 +49,19 @@ public class ListWidgetProvider extends AppWidgetProvider {
         // content providers, the data is often updated via a background service, or in response to
         // user interaction in the main app.  To ensure that the widget always reflects the current
         // state of the data, we must listen for changes and update ourselves accordingly.
-        final ContentResolver r = context.getContentResolver();
-        if (sDataObserver == null) {
-            final AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-            final ComponentName cn = new ComponentName(context, ListWidgetProvider.class);
-            sDataObserver = new WeatherDataProviderObserver(mgr, cn, sWorkerQueue);
-            r.registerContentObserver(NotePad.Notes.CONTENT_URI, true, sDataObserver);
-        }
     }
 
     @Override
-    public void onReceive(Context ctx, Intent intent) {
+    public void onReceive(Context context, Intent intent) {
         final String action = intent.getAction();
         //if (action.equals(REFRESH_ACTION)) {
         if (action.equals(OPEN_ACTION)) {
-        	Toast.makeText(ctx, "OpenAction", Toast.LENGTH_SHORT).show();
+        	Intent appIntent = new Intent();
+        	appIntent.setClass(context, FragmentLayout.class);
+        	appIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        	context.startActivity(appIntent);
         } else if (action.equals(CREATE_ACTION)) {
-        	Toast.makeText(ctx, "CreateAction", Toast.LENGTH_SHORT).show();
+        	Toast.makeText(context, "CreateAction", Toast.LENGTH_SHORT).show();
         	
             // BroadcastReceivers have a limited amount of time to do work, so for this sample, we
             // are triggering an update of the data on another thread.  In practice, this update
@@ -147,15 +100,14 @@ public class ListWidgetProvider extends AppWidgetProvider {
             final int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
                     AppWidgetManager.INVALID_APPWIDGET_ID);
             final long noteId = intent.getLongExtra(EXTRA_NOTE_ID, -1);
-            Toast.makeText(ctx, "NoteId = " + noteId, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "NoteId = " + noteId, Toast.LENGTH_SHORT).show();
         }
 
-        super.onReceive(ctx, intent);
+        super.onReceive(context, intent);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-    	Log.d(TAG, "onUpdate");
     	// This is not called on start up if we are using a configuration activity
     	
         // Update each of the widgets with the remote adapter
@@ -189,6 +141,8 @@ public class ListWidgetProvider extends AppWidgetProvider {
             final PendingIntent openAppPendingIntent = PendingIntent.getBroadcast(context, 0,
             		openAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
             rv.setOnClickPendingIntent(R.id.openAppButton, openAppPendingIntent);
+         // Bind the click intent for the button on the widget
+            rv.setOnClickPendingIntent(R.id.titleButton, openAppPendingIntent);
             // Create button
             final Intent createIntent = new Intent(context, ListWidgetProvider.class);
             createIntent.setAction(ListWidgetProvider.CREATE_ACTION);
