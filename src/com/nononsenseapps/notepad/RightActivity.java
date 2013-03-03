8be6982d14ce234b741bdafe31faed6ee9f36b83
Jonas Kalderstam <jonas@kalderstam.se>
2013-03-03 17:38:34 +0100

    New-button now visible in editor on phones.
    fixes #221
    
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- src/com/nononsenseapps/notepad/RightActivity.java
+++ src/com/nononsenseapps/notepad/RightActivity.java
@@ -1,8 +1,20 @@
 package com.nononsenseapps.notepad;
 
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.nononsenseapps.helpers.SyncHelper;
+import com.nononsenseapps.helpers.dualpane.DualLayoutActivity;
+import com.nononsenseapps.helpers.dualpane.DualLayoutActivity.CONTENTVIEW;
+import com.nononsenseapps.notepad.prefs.MainPrefs;
+import com.nononsenseapps.notepad.prefs.SyncPrefs;
+
 /**
  * Convenience class where external edits are opened.
- *
+ * 
  */
 public class RightActivity extends MainActivity {
 
@@ -14,10 +26,16 @@ public class RightActivity extends MainActivity {
 		finish();
 		super.goUp();
 	}
-	
+
 	/**
 	 * Dont sync in editor
 	 */
 	protected void syncOnStart() {
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.right_activity_menu, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
 }
