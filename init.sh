0a05c26108f3dddf3d0da2357faad6f90b2ed2ba
Jonas Kalderstam <jonas@kalderstam.se>
2013-05-01 17:42:21 +0200

    All waiting changes
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- init.sh
+++ init.sh
@@ -12,11 +12,20 @@ VIEWPAGERDIR="Android-ViewPagerIndicator/library"
 BETTERPICKDIR="android-betterpickers/library"
 DRAGSORTLIST="drag-sort-listview/library"
 WIZARD="wizardpager"
+NINEOLD="NineOldAndroids/library"
+SHOWCASE="ShowcaseView/library"
 UTILS="Utils"
-
 NOTES="NoNonsenseNotes"
 
 # The following is to make it all buildable with ant
+#Makes sure everyone has the same support library
+rm $BASEDIR/$NINEOLD/libs/android-support-v4.jar
+rm $BASEDIR/$SHOWCASE/libs/android-support-v4.jar
+rm $BASEDIR/$BETTERPICKDIR/libs/android-support-v4.jar
+
+cp $BASEDIR/$NOTES/libs/android-support-v4.jar $BASEDIR/$NINEOLD/libs/
+cp $BASEDIR/$NOTES/libs/android-support-v4.jar $BASEDIR/$SHOWCASE/libs
+cp $BASEDIR/$NOTES/libs/android-support-v4.jar $BASEDIR/$BETTERPICKDIR/libs/
 
 # Viewpager
 $UPDATELIB -p $BASEDIR/$VIEWPAGERDIR $TARGET
@@ -30,6 +39,10 @@ $UPDATELIB -p $BASEDIR/$DRAGSORTLIST $TARGET
 
 #wizard pager
 $UPDATELIB -p $BASEDIR/$WIZARD $TARGET
+
+#ShowCaseView
+$UPDATELIB -p $BASEDIR/$NINEOLD $TARGET
+$UPDATELIB -p $BASEDIR/$SHOWCASE $TARGET
 
 #Utils library
 $UPDATELIB -p $BASEDIR/$UTILS $TARGET
