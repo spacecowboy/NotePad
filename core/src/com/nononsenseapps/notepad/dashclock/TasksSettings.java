7f98df2431a6f4997e775e62d94bb43f51cd4527
Jonas Kalderstam <jonas@kalderstam.se>
2014-04-11 13:34:26 +0200

    Make Dashclock configuration activity a dialog on large screens

--- core/src/com/nononsenseapps/notepad/dashclock/TasksSettings.java
+++ core/src/com/nononsenseapps/notepad/dashclock/TasksSettings.java
@@ -1,53 +1,84 @@
+/*
+ * Copyright (c) 2014 Jonas Kalderstam.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nononsenseapps.notepad.dashclock;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.provider.BaseColumns;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.MenuItem;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.nononsenseapps.notepad.core.R;
-import com.nononsenseapps.notepad.database.TaskList;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
  * handset devices, settings are presented as a single list. On tablets,
  * settings are split by category, with category headers shown to the left of
  * the list of settings.
- * <p>
+ * <p/>
  * See <a href="http://developer.android.com/design/patterns/settings.html">
  * Android Design: Settings</a> for design guidelines and the <a
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class TasksSettings extends PreferenceActivity {
-	
-	/**
-	 * Determines whether to always show the simplified settings UI, where
-	 * settings are presented in a single list. When false, settings are shown
-	 * as a master/detail two-pane view on tablets. When true, a single pane is
-	 * shown on tablets.
-	 */
-	private static final boolean ALWAYS_SIMPLE_PREFS = false;
-	
-	public void onCreate(Bundle savedInstanceState) {
+public class TasksSettings extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setupFauxDialog();
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_dashclock_settings);
+
+        setupActionBar();
+    }
+
+    protected void setupFauxDialog() {
+        // Check if this should be a dialog
+        TypedValue tv = new TypedValue();
+        if (!getTheme().resolveAttribute(R.attr.isDialog, tv, true) ||
+            tv.data == 0) {
+            return;
+        }
+
+        // Should be a dialog; set up the window parameters.
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.width = getResources()
+                .getDimensionPixelSize(R.dimen.configure_dialog_width);
+        params.height = Math.min(getResources()
+                .getDimensionPixelSize(R.dimen.configure_dialog_max_height),
+                dm.heightPixels * 3 / 4);
+        params.alpha = 1.0f;
+        params.dimAmount = 0.5f;
+        getWindow().setAttributes(params);
+    }
+
+    protected void setupActionBar() {
         getActionBar().setIcon(R.drawable.ic_stat_notification_edit);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setTitle(R.string.dashclock_title_activity_tasks_settings);
     }
-	@Override
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
@@ -56,133 +87,4 @@ public class TasksSettings extends PreferenceActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-		setupSimplePreferencesScreen();
-	}
-
-	/**
-	 * Shows the simplified settings UI if the device configuration if the
-	 * device configuration dictates that a simplified, single-pane UI should be
-	 * shown.
-	 */
-	@SuppressWarnings("deprecation")
-	private void setupSimplePreferencesScreen() {
-		// In the simplified UI, fragments are not used at all and we instead
-		// use the older PreferenceActivity APIs.
-
-		// Add 'general' preferences.
-		addPreferencesFromResource(R.xml.dashclock_pref_general);
-
-		// Bind the summaries of EditText/List/Dialog/Ringtone preferences to
-		// their values. When their values change, their summaries are updated
-		// to reflect the new value, per the Android Design guidelines.
-		bindPreferenceSummaryToValue(findPreference("list_spinner"));
-		setEntries(this, (ListPreference) findPreference("list_spinner"));
-		
-		bindPreferenceSummaryToValue(findPreference("list_due_upper_limit"));
-	}
-	
-	/**
-	 * Reads the lists from database. Also adds "All lists" as the first item.
-	 * 
-	 * @return
-	 */
-	private static void setEntries(Activity activity, ListPreference listSpinner) {
-
-		ArrayList<CharSequence> entries = new ArrayList<CharSequence>();
-		ArrayList<CharSequence> values = new ArrayList<CharSequence>();
-
-		// Start with all lists
-		entries.add("All lists");
-		values.add("-1");
-		// Set it as the default value also
-		//listSpinner.setDefaultValue("-1");
-
-		Cursor cursor = activity.getContentResolver().query(
-				TaskList.URI,
-				TaskList.Columns.FIELDS, null, null,
-				TaskList.Columns.TITLE);
-		if (cursor != null) {
-			if (!cursor.isClosed() && !cursor.isAfterLast()) {
-				while (cursor.moveToNext()) {
-					entries.add(cursor.getString(1));
-					values.add(Long.toString(cursor.getLong(0)));
-				}
-			}
-
-			cursor.close();
-		}
-
-		// Set the values
-		if (listSpinner != null) {
-			listSpinner.setEntries(entries.toArray(new CharSequence[entries
-					.size()]));
-			listSpinner.setEntryValues(values.toArray(new CharSequence[values
-					.size()]));
-
-			listSpinner.setSummary(listSpinner.getEntry());
-		}
-	}
-
-    /** {@inheritDoc} */
-    @Override
-    public boolean isValidFragment(final String name) {
-        return false;
-    }
-
-	/**
-	 * A preference value change listener that updates the preference's summary
-	 * to reflect its new value.
-	 */
-	private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-		@Override
-		public boolean onPreferenceChange(Preference preference, Object value) {
-			String stringValue = value.toString();
-
-			if (preference instanceof ListPreference) {
-				// For list preferences, look up the correct display value in
-				// the preference's 'entries' list.
-				ListPreference listPreference = (ListPreference) preference;
-				int index = listPreference.findIndexOfValue(stringValue);
-
-				// Set the summary to reflect the new value.
-				preference
-						.setSummary(index >= 0 ? listPreference.getEntries()[index]
-								: null);
-
-			}  else {
-				// For all other preferences, set the summary to the value's
-				// simple string representation.
-				preference.setSummary(stringValue);
-			}
-			return true;
-		}
-	};
-
-	/**
-	 * Binds a preference's summary to its value. More specifically, when the
-	 * preference's value is changed, its summary (line of text below the
-	 * preference title) is updated to reflect the value. The summary is also
-	 * immediately updated upon calling this method. The exact display format is
-	 * dependent on the type of preference.
-	 * 
-	 * @see #sBindPreferenceSummaryToValueListener
-	 */
-	private static void bindPreferenceSummaryToValue(Preference preference) {
-		// Set the listener to watch for value changes.
-		preference
-				.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-
-		// Trigger the listener immediately with the preference's
-		// current value.
-		sBindPreferenceSummaryToValueListener.onPreferenceChange(
-				preference,
-				PreferenceManager.getDefaultSharedPreferences(
-						preference.getContext()).getString(preference.getKey(),
-						""));
-	}
 }
