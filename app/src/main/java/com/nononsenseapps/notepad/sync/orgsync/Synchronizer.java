f38b0c02a10591337f8a93db903004269e0de12f
Jonas Kalderstam <jonas@kalderstam.se>
2014-08-05 14:30:30 +0200

    Fix for reported crash. Likely the note has been deleted on both ends.

--- app/src/main/java/com/nononsenseapps/notepad/sync/orgsync/Synchronizer.java
+++ app/src/main/java/com/nononsenseapps/notepad/sync/orgsync/Synchronizer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014 Jonas Kalderstam.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nononsenseapps.notepad.sync.orgsync;
 
 import android.content.Context;
@@ -267,9 +283,10 @@ public abstract class Synchronizer extends DBSyncBase implements
 					// DELETE NODE DB
 					//Log.d(TAG, "DELETE NODE DB");
 					deleteLocal(task, dbEntry);
-					deleteNode(node);
-
-					shouldUpdateFile = true;
+                    if (node != null) {
+                        deleteNode(node);
+                        shouldUpdateFile = true;
+                    }
 				} else {
 					if (node == null) {
 						// DELETE DB TASK
