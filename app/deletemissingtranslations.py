ac96d5b795a8edc16f904dcd96d649abe9721ee2
Jonas Kalderstam <jonas@kalderstam.se>
2014-12-17 01:21:59 +0100

    Update translation scripts
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- app/deletemissingtranslations.py
+++ app/deletemissingtranslations.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python
+#    Copyright 2014 Jonas Kalderstam
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 Removes translations which do not exceed
 50% translated strings (roughly)
@@ -55,6 +68,6 @@ for dir in dirs:
 
         print(lang, length)
 
-        if length < 0.6 * englishlength:
+        if length < 0.4 * englishlength:
             print("Delete", lang)
             os.remove(fpath)
--- app/listlang.py
+++ app/listlang.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python
+#    Copyright 2014 Jonas Kalderstam
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 Lists the values directories for languages. Then
 inserts them in arrays.xml
@@ -21,6 +34,14 @@ for dir in dirs:
     m = re.match('^values\-([a-z]{2})(-r([A-Z]{2}))?$', dir)
 
     if m is not None:
+        # If no file exists, then remove it
+        hasfile = False
+        for f in os.listdir('./src/main/res/' + dir):
+            if f.endswith('.xml'):
+                hasfile = True
+                break
+        if not hasfile:
+            continue
         # Language iso code
         isocode = m.group(1)
         if m.group(3) is not None:
