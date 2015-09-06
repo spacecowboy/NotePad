53d4a250b50dc943e986ce614a9a514da84e0a99
Jonas Kalderstam <jonas@kalderstam.se>
2015-09-06 18:51:45 +0200

    Material base

--- app/src/play/java/com/nononsenseapps/filepicker/DropboxFilePickerFragment.java
+++ app/src/play/java/com/nononsenseapps/filepicker/DropboxFilePickerFragment.java
@@ -1,37 +1,37 @@
 /*
- * Copyright (c) 2014 Jonas Kalderstam.
+ * Copyright (c) 2015 Jonas Kalderstam
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nononsenseapps.filepicker;
 
 import android.annotation.SuppressLint;
-import android.content.AsyncTaskLoader;
-import android.content.Loader;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.util.SortedListAdapterCallback;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.exception.DropboxException;
-import com.nononsenseapps.notepad.R;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 @SuppressLint("ValidFragment")
 public class DropboxFilePickerFragment
@@ -52,8 +52,9 @@ public class DropboxFilePickerFragment
         this.dbApi = api;
     }
 
+    @Override
     public void onNewFolder(final String name) {
-        File folder = new File(currentPath.path, name);
+        File folder = new File(mCurrentPath.path, name);
 
         if (folderCreator == null) {
             folderCreator = new FolderCreator();
@@ -62,37 +63,19 @@ public class DropboxFilePickerFragment
         folderCreator.execute(folder.getPath());
     }
 
-    private class FolderCreator extends AsyncTask<String, Void, Void> {
-
-        @Override
-        protected Void doInBackground(final String... paths) {
-            for (String path : paths) {
-                try {
-                    dbApi.createFolder(path);
-                    currentPath = dbApi.metadata(path, 1, null, false, null);
-                    refresh();
-                } catch (DropboxException e) {
-                    Toast.makeText(getActivity(), R.string.create_folder_error,
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-            return null;
-        }
-    }
-
     @Override
-    protected boolean isDir(final DropboxAPI.Entry file) {
+    public boolean isDir(final DropboxAPI.Entry file) {
         return file.isDir;
     }
 
     @Override
-    protected DropboxAPI.Entry getParent(final DropboxAPI.Entry from) {
+    public DropboxAPI.Entry getParent(final DropboxAPI.Entry from) {
         // Take care of a slight limitation in Dropbox code:
         if (from.path.length() > 1 && from.path.endsWith("/")) {
             from.path = from.path.substring(0, from.path.length() - 1);
         }
         String parent = from.parentPath();
-        if (parent.isEmpty()) {
+        if (TextUtils.isEmpty(parent)) {
             parent = "/";
         }
 
@@ -101,7 +84,7 @@ public class DropboxFilePickerFragment
     }
 
     @Override
-    protected DropboxAPI.Entry getPath(final String path) {
+    public DropboxAPI.Entry getPath(final String path) {
         final DropboxAPI.Entry entry = new DropboxAPI.Entry();
         entry.path = path;
         entry.isDir = true;
@@ -110,68 +93,78 @@ public class DropboxFilePickerFragment
     }
 
     @Override
-    protected String getFullPath(final DropboxAPI.Entry file) {
+    public String getFullPath(final DropboxAPI.Entry file) {
         return file.path;
     }
 
     @Override
-    protected String getName(final DropboxAPI.Entry file) {
+    public String getName(final DropboxAPI.Entry file) {
         return file.fileName();
     }
 
     @Override
-    protected DropboxAPI.Entry getRoot() {
+    public DropboxAPI.Entry getRoot() {
         return getPath("/");
     }
 
     @Override
-    protected Uri toUri(final DropboxAPI.Entry file) {
-        return new Uri.Builder().scheme("dropbox").path(file.path).build();
+    public Uri toUri(final DropboxAPI.Entry file) {
+        return new Uri.Builder().scheme("dropbox").authority("").path(file.path).build();
     }
 
     @Override
-    protected Comparator<DropboxAPI.Entry> getComparator() {
-        return new Comparator<DropboxAPI.Entry>() {
-            @Override
-            public int compare(final DropboxAPI.Entry lhs,
-                               final DropboxAPI.Entry rhs) {
-                if (isDir(lhs) && !isDir(rhs)) {
-                    return -1;
-                } else if (isDir(rhs) && !isDir(lhs)) {
-                    return 1;
-                } else {
-                    return lhs.fileName().toLowerCase()
-                            .compareTo(rhs.fileName().toLowerCase());
-                }
-            }
-        };
-    }
-
-    @Override
-    protected Loader<List<DropboxAPI.Entry>> getLoader() {
-        return new AsyncTaskLoader<List<DropboxAPI.Entry>>(getActivity()) {
+    public Loader<SortedList<DropboxAPI.Entry>> getLoader() {
+        return new AsyncTaskLoader<SortedList<DropboxAPI.Entry>>(getActivity()) {
 
             @Override
-            public List<DropboxAPI.Entry> loadInBackground() {
-                ArrayList<DropboxAPI.Entry> files =
-                        new ArrayList<DropboxAPI.Entry>();
+            public SortedList<DropboxAPI.Entry> loadInBackground() {
+                SortedList<DropboxAPI.Entry> files = new SortedList<>(DropboxAPI.Entry.class,
+                        new SortedListAdapterCallback<DropboxAPI.Entry>(getAdapter()) {
+                            @Override
+                            public int compare(DropboxAPI.Entry lhs, DropboxAPI.Entry rhs) {
+                                if (isDir(lhs) && !isDir(rhs)) {
+                                    return -1;
+                                } else if (isDir(rhs) && !isDir(lhs)) {
+                                    return 1;
+                                } else {
+                                    return lhs.fileName().toLowerCase()
+                                            .compareTo(rhs.fileName().toLowerCase());
+                                }
+                            }
+
+                            @Override
+                            public boolean areContentsTheSame(DropboxAPI.Entry lhs, DropboxAPI.Entry rhs) {
+                                return lhs.fileName().equals(rhs.fileName()) && (lhs.isDir == rhs.isDir);
+                            }
+
+                            @Override
+                            public boolean areItemsTheSame(DropboxAPI.Entry lhs, DropboxAPI.Entry rhs) {
+                                return areContentsTheSame(lhs, rhs);
+                            }
+                        }, 0);
+
                 try {
 
-                    if (!dbApi.metadata(currentPath.path, 1, null, false,
+                    if (!dbApi.metadata(mCurrentPath.path, 1, null, false,
                             null).isDir) {
-                        currentPath = getRoot();
+                        mCurrentPath = getRoot();
                     }
 
                     DropboxAPI.Entry dirEntry =
-                            dbApi.metadata(currentPath.path, 0, null, true,
+                            dbApi.metadata(mCurrentPath.path, 0, null, true,
                                     null);
+
+                    files.beginBatchedUpdates();
+
                     for (DropboxAPI.Entry entry : dirEntry.contents) {
                         if ((mode == MODE_FILE || mode == MODE_FILE_AND_DIR) ||
                                 entry.isDir) {
                             files.add(entry);
                         }
                     }
-                } catch (DropboxException e) {
+
+                    files.endBatchedUpdates();
+                } catch (DropboxException ignored) {
                 }
 
                 return files;
@@ -184,8 +177,8 @@ public class DropboxFilePickerFragment
             protected void onStartLoading() {
                 super.onStartLoading();
 
-                if (currentPath == null || !currentPath.isDir) {
-                    currentPath = getRoot();
+                if (mCurrentPath == null || !mCurrentPath.isDir) {
+                    mCurrentPath = getRoot();
                 }
 
                 forceLoad();
@@ -201,5 +194,21 @@ public class DropboxFilePickerFragment
         };
     }
 
+    private class FolderCreator extends AsyncTask<String, Void, Void> {
 
+        @Override
+        protected Void doInBackground(final String... paths) {
+            for (String path : paths) {
+                try {
+                    dbApi.createFolder(path);
+                    mCurrentPath = dbApi.metadata(path, 1, null, false, null);
+                    refresh();
+                } catch (DropboxException e) {
+                    Toast.makeText(getActivity(), R.string.nnf_create_folder_error,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            return null;
+        }
+    }
 }
