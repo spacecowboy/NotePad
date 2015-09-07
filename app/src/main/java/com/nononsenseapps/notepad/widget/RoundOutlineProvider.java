0c1e054037402066cb1b9178a02ebf8770fe6069
Jonas Kalderstam <jonas@kalderstam.se>
2015-09-07 19:16:53 +0200

    Style navigation header

---
+++ app/src/main/java/com/nononsenseapps/notepad/widget/RoundOutlineProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Jonas Kalderstam.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.nononsenseapps.notepad.widget;
+
+import android.graphics.Outline;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+
+/**
+ * Creates round outlines for views.
+ */
+public class RoundOutlineProvider extends ViewOutlineProvider {
+
+    private final int mSize;
+
+    public RoundOutlineProvider(int size) {
+        if (0 > size) {
+            throw new IllegalArgumentException("size needs to be > 0. Not " + size);
+        }
+        mSize = size;
+    }
+
+    @Override
+    public final void getOutline(View view, Outline outline) {
+        outline.setOval(0, 0, mSize, mSize);
+    }
+
+}
