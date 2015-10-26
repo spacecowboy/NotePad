805071357bd7646cb5673c9e1c9c330e78c3aa48
Jonas Kalderstam <jonas@kalderstam.se>
2015-10-26 18:09:26 +0100

    Don't need legacy http with Retrofit

--- app/src/main/java/com/nononsenseapps/notepad/sync/googleapi/GoogleTaskSync.java
+++ app/src/main/java/com/nononsenseapps/notepad/sync/googleapi/GoogleTaskSync.java
@@ -35,7 +35,6 @@ import com.nononsenseapps.notepad.database.TaskList;
 import com.nononsenseapps.notepad.prefs.SyncPrefs;
 import com.nononsenseapps.utils.time.RFC3339Date;
 
-import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
 
 import java.io.IOException;
