7da3a344c12b019f73c63aa1c94adfbda408685c
manuel <9112949+CampelloManuel@users.noreply.github.com>
2022-11-02 12:42:16 +0100

    add a new log function

--- app/src/main/java/com/nononsenseapps/helpers/Log.java
+++ app/src/main/java/com/nononsenseapps/helpers/Log.java
@@ -4,9 +4,18 @@ import com.nononsenseapps.build.Config;
 
 public final class Log {
 
+	/**
+	 * Logs the given exception with tag "NNN"
+	 */
+	public static void exception(Exception e) {
+		android.util.Log.e("NNN", e.getMessage());
+
+		String stackTrace = android.util.Log.getStackTraceString(e);
+		android.util.Log.e("NNN", stackTrace);
+	}
+
 	public static void d(String t, String m) {
-		if (Config.LOGGING || true)
-			android.util.Log.d(t, m);
+		if (Config.LOGGING) android.util.Log.d(t, m);
 	}
 
 	public static void e(String tAG, String string, Exception e) {
