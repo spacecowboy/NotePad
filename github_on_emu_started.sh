3141e127b33923c1be29f631def42f57ca3ccc04
manuel <9112949+CampelloManuel@users.noreply.github.com>
2022-11-28 14:13:30 +0100

    move github action to script

---
+++ github_on_emu_started.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+echo "github action script started"
+
+# take screenshot of the host PC after the emulator starts
+# MacOS specific. Ignore in case of error
+screencapture screenshot-desktop.jpg || true
+
+# output may be useful
+adb devices
+
+# close all OS popups in the emulator, like "System UI stopped working"
+adb shell am broadcast -a android.intent.action.CLOSE_SYSTEM_DIALOGS
+
+# close that useless popup that tells you how to exit fullscreen mode
+adb shell settings put secure immersive_mode_confirmations confirmed
+
+# uninstall the app (it can't uninstall it by itself). Ignore any error
+adb shell pm uninstall --user 0 com.nononsenseapps.notepad.test || true
+adb shell pm uninstall --user 0 com.nononsenseapps.notepad || true
+
+# Take a screenshot of the emulator. See also
+# https://developer.android.com/studio/run/advanced-emulator-usage#screenshots
+adb emu screenrecord screenshot ./screenshot-emu-tests-starting.png
+
+# record the tests in the emulator (dies after 3 minutes!)
+# adb emu screenrecord start --bit-rate 100000 --size 540x960 ./emu-video.webm
+
+# declare a function to record 3 minutes & then save the video to the host PC
+record_and_move ()
+{
+  adb shell screenrecord --bit-rate 100000 --size 540x960 /sdcard/video"$1".mp4
+  adb pull /sdcard/video"$1".mp4 .
+}
+
+# call the function 6 times, in series, in background
+{ record_and_move V1 ; record_and_move V2 ; record_and_move V3 ; record_and_move V4 ;  record_and_move V5 ; record_and_move V6 ;  } &
+
+# meanwhile, run tests
+./gradlew connectedCheck
+
+# stop recording. Useless: if the script succeeds, videos are useless.
+# If it fails, the entire script is killed
+# adb emu screenrecord stop
