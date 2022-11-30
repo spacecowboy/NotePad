92f0c03bae097c8e72ddc1c6c9bae04380367c23
Campello Manuel <9112949+CampelloManuel@users.noreply.github.com>
2022-11-30 08:56:00 +0100

    Update github_on_emu_started.sh

--- github_on_emu_started.sh
+++ github_on_emu_started.sh
@@ -2,10 +2,10 @@
 
 # sources:  https://stackoverflow.com/a/66155744/6307322
 #           https://stackoverflow.com/a/62723329/6307322
-# will run all commands, even if one fails, and return the result code of the last one
+# will run all commands, even if one fails, and return the result code of gradle
 echo "github action script started"
 
-# wait a bit for it to finish loading
+# wait a bit for the emulator to finish loading
 sleep 15
 
 # take screenshot of the host PC after the emulator starts
@@ -14,13 +14,13 @@ screencapture screenshot-desktop.jpg
 # output may be useful
 adb devices
 
-# close all OS popups in the emulator, like "System UI stopped working"
+# try to close all OS popups in the emulator, like "System UI stopped working"
 adb shell am broadcast -a android.intent.action.CLOSE_SYSTEM_DIALOGS
 
 # close that useless popup that tells you how to exit fullscreen mode
 adb shell settings put secure immersive_mode_confirmations confirmed
 
-# uninstall the app, if possible. It doesn't uninstall it by itself. Ignore any error
+# manually uninstall the app, if possible. Sometimes it's needed
 adb shell pm uninstall --user 0 com.nononsenseapps.notepad.test
 adb shell pm uninstall --user 0 com.nononsenseapps.notepad
 
@@ -29,23 +29,24 @@ adb shell pm uninstall --user 0 com.nononsenseapps.notepad
 adb emu screenrecord screenshot ./screenshot-emu-tests-starting.png
 
 function getScreenStreamFromEmu() {
-  while true; do
-    # exec-out: run command on emulated android, get the output on the host PC
-    # bitrate & size: to get a smaller video
-    # output-format and - at the end: stream to console output, don't save
-    # bugreport: helpful info when a video starts
-    # alternative: `adb emu screenrecord start --bit-rate 100000 --size 540x960 ./video.webm`,
-    #   but the tool is hardcoded to die after 3 minutes. Thanks Google.
-    # returns: 0 if 3 minutes passed, a number > 0 if there was an error
-    adb exec-out screenrecord --output-format=h264 --bit-rate 100000 --size 480x854 --bugreport -
-    # should go to STDERR, without polluting the video stream, hopefully    
-    echo "restarting screenrecord from adb, PID = " $! " and " $? " is return code" >&2
-    if [ $? -eq 255 ] ; then
-	    # the emulator died: exit the loop & the function. 
-      # Hopefully this also stops ffmpeg gracefully
-	    break
-    fi
-  done
+	while true; do
+	# exec-out: run command on emulated android, get the output on the host PC
+	# bitrate & size: to get a smaller video
+	# output-format and - at the end: stream to console output, don't save
+	# bugreport: helpful info when a video starts
+	# alternative: `adb emu screenrecord start --bit-rate 100000 --size 540x960 ./vid.webm`,
+	#   but the tool is hardcoded to die after 3 minutes. Thanks Google.
+	# returns: 0 if 3 minutes passed, a number > 0 if there was an error
+	adb exec-out screenrecord --output-format=h264 --bit-rate 100000 --size 480x854 --bugreport -
+	ADB_RET_CODE=$?
+	# should go to STDERR, without polluting the video stream, hopefully    
+	echo "restarting screenrecord, PID = " $! " and " $ADB_RET_CODE " is return code" >&2
+		if [ $ADB_RET_CODE -eq 255 ] ; then
+			# the emulator died: exit the loop & the function. 
+			# Hopefully this also stops ffmpeg gracefully
+			break
+		fi
+	done
 }
 
 # TODO android API 32 images have a built-in screen record feature that can make long videos, but you have to start it from the top drawer menu. Check if API 23 has it, and try to use it with ADB. Videos go on /sdcard/movies/
@@ -60,12 +61,9 @@ echo "---------- all OK as of now ----------"
 VIDEO_PID=$!
 
 # press "HOME" to close any "system UI not responding" popups still showing.
-# For codes, see https://stackoverflow.com/a/8483797/6307322
 # Notice that these popups appear when the host PC is too slow, so the best solution
 # is to run the tests under a different host OS, device skin, API version, ...
 adb shell input keyevent 3
-# The numbers are X and Y coordinates, in pixels, so 480 854 for the bottom right corner
-# adb shell input tap 150 440
 
 # run tests
 ./gradlew connectedCheck
@@ -82,6 +80,10 @@ echo "killing process id=" $VIDEO_PID
 kill -s QUIT $VIDEO_PID
 sleep 10
 sudo kill -s QUIT $VIDEO_PID
+sleep 10
+kill $VIDEO_PID
+sleep 10
+sudo kill $VIDEO_PID
 sleep 10
 echo "--- try the last 2 ---"
 killall -INT  ffmpeg
